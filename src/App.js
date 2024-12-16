f887b3df095f6b34cb238ab53b2a64a44b925cd5
sara <ektsara@gmail.com>
2024-12-16 16:31:40 +0100

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
