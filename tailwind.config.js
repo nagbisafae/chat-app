9c68f95cbb6494f430aa97f65413b97801fb7535
sara <ektsara@gmail.com>
2024-12-18 16:13:02 +0100

    2nd changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
       colors: {
         customBlue: "#2B616D",
         BlueVert: "#B2DBD5",
-        gris: "#797C7B", // Ajoute un nom pour ta couleur
+        gris: "#797C7B",
+        orange: "#FA8D62",
+        orangee: "#FB8C00", // Ajoute un nom pour ta couleur
       },
       spacing: {
-        "60px": "60px", // Ajoute la valeur 120px
+        "60px": "60px",
+        "120px": "120px",
+        "400px": "400px",
+        cust: "226px",
+        // Ajoute la valeur 120px
+      },
+      width: {
+        custom: "450px",
+        custom1: "500px",
+        custom2: "550px",
+        custom3: "650px",
+        custom4: "225px",
+      },
+      top: {
+        cus: "225px",
       },
     },
   },
