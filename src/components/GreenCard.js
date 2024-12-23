141b6336f8f50b2bdcd8865c47daf2b71e7d53e3
nagbisafae <safae.claritytb@gmail.com>
2024-12-23 16:07:02 +0100

    Last Changes

--- src/components/GreenCard.js
+++ src/components/GreenCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../App.css";
 import { motion } from "framer-motion";
 import logo from "../assets/LOGO.png";
-// import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function GreenCard({
@@ -13,15 +12,18 @@ function GreenCard({
   roundedClass,
   btnhref,
   att,
+  alignment, // 'right' or 'left'
 }) {
   const navigate = useNavigate();
 
   return (
     <motion.div
-      className={`bg-customBlue h-screen w-[550px] ${roundedClass} flex justify-center pt-12 ${att}`}
-      initial={{ opacity: 0, x: "100%" }}
+      className={`bg-customBlue h-screen w-1/3 ${roundedClass} flex justify-center items-center absolute ${
+        alignment === "left" ? "left-0" : "right-0"
+      } ${att}`}
+      initial={{ opacity: 0, x: alignment === "left" ? "-100%" : "100%" }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: "-100%" }}
+      exit={{ opacity: 0, x: alignment === "left" ? "100%" : "-100%" }}
       transition={{ duration: 0.5 }}
     >
       <div className="flex flex-col justify-center items-center">
@@ -36,45 +38,16 @@ function GreenCard({
           <path
             d="M37.7303 100.954C37.3376 100.883 36.9535 100.807 36.5781 100.726"
             stroke="#FA8D62"
-            stroke-width="1.50643"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.50643"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
-          <path
-            d="M30.9311 99.0733C5.29562 89.2078 15.2341 56.1235 36.9182 63.8495C37.7509 64.1659 38.4867 64.6607 39.0626 65.2916C39.6385 65.9225 40.0374 66.6706 40.2251 67.472C40.4229 68.2615 40.4252 69.081 40.2318 69.8714C40.0385 70.6618 39.6544 71.4034 39.1071 72.0428C37.9297 73.3542 36.3085 74.2765 34.4949 74.6668C32.6812 75.057 30.7764 74.8935 29.0756 74.2015C14.7475 68.6289 24.673 45.8998 41.9982 42.0999"
-            stroke="#FA8D62"
-            stroke-width="1.50643"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M44.9314 41.6499C45.3202 41.6156 45.7129 41.591 46.1095 41.576"
-            stroke="#FA8D62"
-            stroke-width="1.50643"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M40.5596 23.7152C40.2982 23.4506 40.1301 23.1209 40.0771 22.7687C40.0241 22.4165 40.0886 22.058 40.2624 21.7394C40.4361 21.4208 40.7111 21.1568 41.0518 20.9815C41.3924 20.8061 41.7832 20.7275 42.1736 20.7557L106.616 25.4003C107.017 25.4296 107.399 25.5703 107.708 25.8034C108.018 26.0365 108.242 26.3506 108.348 26.7029C108.454 27.0553 108.439 27.4288 108.303 27.7727C108.167 28.1166 107.918 28.4142 107.589 28.625L54.7451 62.4202C54.4251 62.6252 54.0447 62.7384 53.6531 62.7451C53.2614 62.7518 52.8766 62.6517 52.5483 62.4578C52.2199 62.2639 51.9633 61.9851 51.8114 61.6575C51.6595 61.3298 51.6194 60.9683 51.6962 60.6197L56.1023 40.5123C56.1619 40.2418 56.1512 39.9623 56.0711 39.6962C55.9909 39.43 55.8435 39.1845 55.6405 38.9791L40.5596 23.7152Z"
-            fill="white"
-            stroke="#45413C"
-            stroke-width="1.50643"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M56.103 40.5121L51.6969 60.6195C51.6207 60.9679 51.6612 61.3291 51.8133 61.6565C51.9654 61.9838 52.222 62.2623 52.5501 62.4561C52.8782 62.6498 53.2627 62.7499 53.6541 62.7435C54.0454 62.7371 54.4257 62.6244 54.7459 62.42L107.591 28.6247C107.92 28.4138 108.17 28.1159 108.306 27.7715C108.442 27.4271 108.458 27.0531 108.352 26.7002L56.0712 39.6963C56.1519 39.9622 56.1628 40.2416 56.103 40.5121Z"
-            fill="#DAEDF7"
-            stroke="#45413C"
-            stroke-width="1.50643"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
+          {/* Add other SVG paths here */}
         </svg>
         <div>
-          <p class="font-serif text-white text-5xl pl-12 ">
+          <p className="font-serif text-white text-5xl pl-12">
             {maintxt}
-            <br></br>
+            <br />
             {maintxt1}
           </p>
 
@@ -84,13 +57,13 @@ function GreenCard({
             className="object-contain h-24 pt-2 pl-16"
           />
         </div>
-        <div class="flex justify-center items-center">
-          <p class="text-white font-serif pt-4 ">{smtxt}</p>
+        <div className="flex justify-center items-center">
+          <p className="text-white font-serif mt-20 pt-4">{smtxt}</p>
         </div>
-        <div class="flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <button
             onClick={() => navigate(btnhref)}
-            class="hover:bg-orange transition duration-150 ease-out hover:ease-in bg-white h-10 text-customBlue rounded-full drop-shadow-lg  font-serif w-72 mt-8 text-center font-semibold text-sm border-2 border-orange"
+            className="hover:bg-orange hover:text-white transition duration-150 ease-out hover:ease-in bg-white h-10 text-customBlue rounded-full drop-shadow-lg font-serif w-72 mt-8 text-center font-semibold text-sm border-2 border-BlueVert"
           >
             {btntxt}
           </button>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -42,9 +42,9 @@ function Login() {
 
   return (
     <>
-      <div className="custom-scrollbar bg-gradient-to-b from-BlueVert to-[#F3F6F6] custom-scrollbar">
+      <div className="custom-scrollbar bg-gradient-to-b overflow-y-auto from-BlueVert to-[#F3F6F6]">
         <div className="flex justify-between ">
-          <div class=" bg-gradient-to-b w-custom7 from-BlueVert to-[#F3F6F6] min-h-screen ">
+        <div className="h-screen flex items-center justify-center lg:w-2/3 w-custom7">
             <motion.div
               initial={{ opacity: 0, x: "-100%" }}
               animate={{ opacity: 1, x: 0 }}
@@ -55,87 +55,70 @@ function Login() {
                 <img src={girl} alt="welcome" className="w-56 h-48 mt-4" />
               </div>
               <div class=" flex justify-center items-center ">
-                <label class="font-serif font-semibold text-customBlue leading-4 text-3xl mt-12 inline-block ">
+                <label class="font-serif font-semibold text-customBlue text-3xl inline-block mb-4">
                   Welcome Back!
                 </label>
               </div>
               <div class=" flex justify-center items-center ">
-                <div class="  mt-10    lg:drop-shadow-lg lg:rounded-3xl lg:border lg:border-BlueVert-2 lg:w-96  lg:bg-BlueVert lg:bg-opacity-50 ">
+                <div class="lg:drop-shadow-lg lg:rounded-3xl lg:border lg:border-BlueVert-2 lg:w-96 lg:h-[500px] lg:bg-BlueVert lg:bg-opacity-50 ">
                   {/* Formulaire   */}
-
-                  <form class="relative" onSubmit={handleLogin}>
+                  <form class="relative mt-10" onSubmit={handleLogin}>
                     <div class="flex justify-center items-center">
-                      <InputField
+                    <InputField
                         label="Email:"
                         type="email"
                         placeholder="Enter your email"
                         col="text-gris"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        svg={
+                          <svg
+                            width="13"
+                            height="13"
+                            viewBox="0 0 13 13"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              opacity="0.4"
+                              d="M4.99376 0.887503L10.7322 3.97907C13.3064 5.36595 13.3064 7.63406 10.7322 9.02093L4.99376 12.1125C1.13241 14.1928 -0.442967 12.4881 1.48771 8.33472L2.07093 7.08509C2.23853 6.7167 2.23853 6.29053 2.07093 5.92214L1.48771 4.66528C-0.442967 0.511893 1.13911 -1.1928 4.99376 0.887503Z"
+                              fill="#2B616D"
+                            />
+                          </svg>
+                        }
                       />
-                      <svg
-                        class="absolute left-300px top-16"
-                        width="13"
-                        height="13"
-                        viewBox="0 0 13 13"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          opacity="0.4"
-                          d="M4.99376 0.887503L10.7322 3.97907C13.3064 5.36595 13.3064 7.63406 10.7322 9.02093L4.99376 12.1125C1.13241 14.1928 -0.442967 12.4881 1.48771 8.33472L2.07093 7.08509C2.23853 6.7167 2.23853 6.29053 2.07093 5.92214L1.48771 4.66528C-0.442967 0.511893 1.13911 -1.1928 4.99376 0.887503Z"
-                          fill="#2B616D"
-                        />
-                      </svg>
                     </div>
                     <div class="flex justify-center items-center">
-                      <InputField
-                        label="Password:"
-                        type="password"
-                        placeholder="Enter your password"
-                        col="text-gris"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                      <svg
-                        class="absolute left-300px top-36"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 14 14"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M3.40015 5.8V4.6C3.40015 2.614 4.00015 1 7.00015 1C10.0001 1 10.6001 2.614 10.6001 4.6V5.8"
-                          stroke="#2B616D"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          d="M10 13H4C1.6 13 1 12.4 1 10V8.8C1 6.4 1.6 5.8 4 5.8H10C12.4 5.8 13 6.4 13 8.8V10C13 12.4 12.4 13 10 13Z"
-                          stroke="#2B616D"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          d="M9.39819 9.39996H9.40353"
-                          stroke="#2B616D"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          d="M6.99756 9.39996H7.00296"
-                          stroke="#2B616D"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          d="M4.59668 9.39999H4.60207"
-                          stroke="#2B616D"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
+                    <InputField
+                      label="Password:"
+                      type="password"
+                      placeholder="Enter your password"
+                      col="text-gris"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      svg={
+                        <svg
+                          width="14"
+                          height="14"
+                          viewBox="0 0 14 14"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M3.40015 5.8V4.6C3.40015 2.614 4.00015 1 7.00015 1C10.0001 1 10.6001 2.614 10.6001 4.6V5.8"
+                            stroke="#2B616D"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                          <path
+                            d="M10 13H4C1.6 13 1 12.4 1 10V8.8C1 6.4 1.6 5.8 4 5.8H10C12.4 5.8 13 6.4 13 8.8V10C13 12.4 12.4 13 10 13Z"
+                            stroke="#2B616D"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      }
+                    />
                     </div>
                     <label class="font-serif font-bold text-customBlue text-sm  mt-8 flex justify-center items-center ">
                       Forgot password ?
@@ -155,15 +138,16 @@ function Login() {
           </div>
 
           <div>
-            <GreenCard
-              att="hidden md:hidden lg:block"
-              roundedClass="rounded-s-3xl"
-              btntxt="Create account"
-              btnhref="/Signup"
-              smtxt="  Reconnect with solutions tailored to your needs."
-              maintxt=" Happy to see"
-              maintxt1="you again at"
-            />
+          <GreenCard
+            att="hidden md:hidden lg:block"
+            roundedClass="rounded-s-3xl"
+            btntxt="Create account"
+            btnhref="/Signup"
+            smtxt="Reconnect with solutions tailored to your needs."
+            maintxt="Happy to see"
+            maintxt1="you again at"
+            alignment="right"
+          />
           </div>
         </div>
       </div>
